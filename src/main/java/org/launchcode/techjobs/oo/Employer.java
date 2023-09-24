0290b2780f30b065da61e315544c7518dd06012d
JillianJaclyn <jillian.fussner@gmail.com>
2023-09-24 16:26:42 -0500

    Trying Autograder

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -1,6 +1,5 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
 
 public class Employer extends JobField {
 
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,6 +1,5 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
 
     public class PositionType extends JobField {
 
